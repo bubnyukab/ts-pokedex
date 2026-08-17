cdc9253b2cad0d60020f486292c9eb4315ff11c8
bubnyukab <bubnyukab@gmail.com>
2026-08-17 13:11:29 +0200

    basic REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,26 @@
+import { createInterface } from "node:readline";
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const cmds = cleanInput(input);
+    if (cmds.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    console.log(`Your command was: ${cmds[0]}`);
+    rl.prompt();
+  });
+}
+
 export function cleanInput(input: string): string[] {
-  const clean = input.trim().toLowerCase().split(/\s+/).filter(Boolean);
-  return clean;
+  return input.trim().toLowerCase().split(/\s+/).filter(Boolean);
 }
